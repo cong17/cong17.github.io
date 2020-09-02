9bded6aa53582f32d495be4e731614359a4f6f5e
congkxtb98 <congkxtb98@gmail.com>
2020-09-02 21:16:37 +0700

    fix-something

---
+++ precache-manifest.5593c760abe5c2f2eb26aa0ee8cd45c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd142ac05520d1a49979af7f600c0ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887758efd81eb1a031e8",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "bab19971805e35a900be",
+    "url": "/static/js/2.f4fb22df.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.f4fb22df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887758efd81eb1a031e8",
+    "url": "/static/js/main.d1b40be1.chunk.js"
+  },
+  {
+    "revision": "1b40c8e3d3c66a89df2c",
+    "url": "/static/js/runtime-main.4a8797e9.js"
+  }
+]);
